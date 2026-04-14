e445092c998787a38f7f61b61f2f72e79f5d9320
Qunst <davorinkunst.1@gmail.com>
2026-04-14 14:17:32 +0200

    main page changes

---
+++ assets/js/tanks-data.js
@@ -0,0 +1,18 @@
+const TANKS = [
+  {
+    slug: "panzer-iv",
+    name: "Panzer IV",
+    nation: "Germany",
+    era: "WW2",
+    type: "Medium tank",
+    featured: true,
+    image: "assets/img/tanks/panzer-iv.jpg",
+    etsyUrl: "https://www.etsy.com/listing/...",
+    fact: "One of the longest-serving German tanks, continuously upgraded during the war.",
+    dimensions: {
+      "1:180": "37 × 17 mm",
+      "1:200": "33 × 15 mm",
+      "1:285": "23 × 11 mm"
+    }
+  }
+];
